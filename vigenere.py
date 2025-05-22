dd12370850aef86854c7c30bb91a3860eb9cd4f9
andreyresende <38325819+andreyresende@users.noreply.github.com>
2025-05-22 11:39:59 -0300

    Adicionando arquivo de Vigenere
    Versão inicial com a função de criptografia e descriptografia prontas, faltando fazer toda a parte de ataque.

---
+++ vigenere.py
@@ -0,0 +1,65 @@
+linhaA = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+linhaB = ['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a']
+linhaC = ['c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b']
+linhaD = ['d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c']
+linhaE = ['e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d']
+linhaF = ['f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e']
+linhaG = ['g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f']
+linhaH = ['h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g']
+linhaI = ['i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h']
+linhaJ = ['j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i']
+linhaK = ['k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j']
+linhaL = ['l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k']
+linhaM = ['m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l']
+linhaN = ['n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m']
+linhaO = ['o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n']
+linhaP = ['p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
+linhaQ = ['q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+linhaR = ['r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q']
+linhaS = ['s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r']
+linhaT = ['t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s']
+linhaU = ['u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
+linhaV = ['v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u']
+linhaW = ['w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v']
+linhaX = ['x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w']
+linhaY = ['y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x']
+linhaZ = ['z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y']
+
+matriz = [linhaA, linhaB, linhaC, linhaD, linhaE, linhaF, linhaG, linhaH, linhaI, linhaJ, linhaK, linhaL, linhaM, 
+          linhaN, linhaO, linhaP, linhaQ, linhaR, linhaS, linhaT, linhaU, linhaV, linhaW, linhaX, linhaY, linhaZ]
+
+print('Digite a mensagem sem espacos e em minusculo')
+mensagem = input()
+print('Digite a chave')
+chave = input()
+
+def vigenere(mensagem, chave):
+    criptograma = ''
+    i = 0
+    for char in mensagem:
+        ascii_msg   = ord(char)-97
+        ascii_chave = ord(chave[i])-97
+        criptograma = criptograma + matriz[ascii_msg][ascii_chave]
+        i = (i+1)%len(chave)
+    return criptograma
+
+def decrypt_vigenere(criptograma, chave):
+    mensagem_original = ''
+    i = 0
+    for char in criptograma:
+        ascii_cript = ord(char)-97
+        ascii_chave = ord(chave[i])-97
+        for linha in matriz:
+            if(linha[ascii_chave] == char):
+                mensagem_original = mensagem_original + linha[0]
+        i = (i+1)%len(chave)
+    return mensagem_original
+
+criptograma = vigenere(mensagem, chave)
+mensagem_original = decrypt_vigenere(criptograma, chave)
+
+print('criptograma = ' + criptograma)
+print('mensagem = ' + mensagem_original)
+
+
+
